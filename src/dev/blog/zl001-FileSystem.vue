81ac34d20a0d1d4d0353571e88ed6abf12fef49f
ZL <zl.sheepcity@gmail.com>
2020-12-15 11:49:49 +0200

    docs: file system

--- src/dev/blog/zl001-FileSystem.vue
+++ src/dev/blog/zl001-FileSystem.vue
@@ -3,9 +3,24 @@
 
         <div class="overline"> 2020.12.15 </div>
         <div class="headline"> File system </div>
+        <br>
 
-        <div class="caption">views</div>
-        ...
+        <h4>views</h4>
+        <p>UX Screens are views</p>
+
+        <h4>parts</h4>
+        <p>Functional parts of the screens</p>
+
+        <h4>bazaar</h4>
+        <p>We will have a deal with things we don't know yet. All things goes to bazaar as communication layers.</p>
+
+        <p>Правило базара: Если что-то нужно — ищи на базаре, если нет торговца или у торговца нет товара — создай.</p>
+
+        <p>Сейчас на базаре есть Content раздающий тексты для полей форм. Должны появиться User, Database и другие.</p>
+
+        <h4>dev</h4>
+        <p>Any files. Use dev/blog to store meaningful info.</p>
+
         <p>zl</p>
 
     </v-container>
